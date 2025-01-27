2a0bc3c5bd974d6e29b027af773694740582341d
Acrylus <theantonjoseph@gmail.com>
2025-01-27 21:36:32 +0800

    Initial Commit

---
+++ src/test/java/com/example/DCP/Inventory/System/DcpInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DCP.Inventory.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcpInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
