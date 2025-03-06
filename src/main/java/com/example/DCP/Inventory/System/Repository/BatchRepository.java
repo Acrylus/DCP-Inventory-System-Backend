39bcbd0f37e5eb8fe4e711213e7490444caf47f5
Acrylus <theantonjoseph@gmail.com>
2025-03-07 01:28:31 +0800

    Cloud Database

--- src/main/java/com/example/DCP/Inventory/System/Repository/BatchRepository.java
+++ src/main/java/com/example/DCP/Inventory/System/Repository/BatchRepository.java
@@ -2,6 +2,8 @@ package com.example.DCP.Inventory.System.Repository;
 
 import com.example.DCP.Inventory.System.Entity.BatchEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BatchRepository extends JpaRepository<BatchEntity, Long> {
 }
--- src/main/java/com/example/DCP/Inventory/System/Repository/UserRepository.java
+++ src/main/java/com/example/DCP/Inventory/System/Repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.DCP.Inventory.System.Repository;
 
 import com.example.DCP.Inventory.System.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
